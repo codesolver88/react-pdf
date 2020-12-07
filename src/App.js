0ea694b2607248e6915b26049d1622ae56958d4a
desh <deshondrock@gmail.com>
2020-12-07 23:58:44 +0530

    modified app and pdfcomponent

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-import PDFComponent from './components/PDFComponent';
+import PDFComponent from "./components/PDFComponent";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <div className="pdfContainer">
           <PDFComponent />
         </div>
--- src/components/PDFComponent.js
+++ src/components/PDFComponent.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf';
-import data from './xls.PDF';
+import React, { Component } from "react";
+import { Document, Page } from "react-pdf";
+import data from "./xls.PDF";
 
 class PDFComponent extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       numPages: null,
       pageNumber: 5,
@@ -12,23 +12,30 @@ class PDFComponent extends Component {
   }
   onDocumentLoad = ({ numPages }) => {
     this.setState({ numPages });
-  }
+  };
+
+  prevHandler = () => {
+    this.setState({ pageNumber: this.state.pageNumber - 1 });
+  };
+
+  nextHandler = () => {
+    this.setState({ pageNumber: this.state.pageNumber + 1 });
+  };
 
   render() {
     const { pageNumber, numPages } = this.state;
 
     return (
       <div>
-        <a href="#" >
-          <img src="./images/test.jpg" width="100" height="100" display="block" alt="image"/>
-        </a>
-        <div id='contentDiv'>
-                  <Document
-                      file={data}
-                      onLoadSuccess={this.onDocumentLoad}>
-                          <Page pageNumber={pageNumber} />
-                  </Document>
-                  <p>Page {pageNumber} of {numPages}</p>
+        <div id="contentDiv">
+          <Document file={data} onLoadSuccess={this.onDocumentLoad}>
+            <Page pageNumber={pageNumber} />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+          <button onClick={this.prevHandler}>Previous</button>
+          <button onClick={this.nextHandler}>Next</button>
         </div>
       </div>
     );
